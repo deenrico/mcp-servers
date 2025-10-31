90337686937ceaf972329ef35e9e6b098fb9844d
Enrico Erler <enrico.erler@proton.me>
2025-10-31 15:49:36 +0100

    update web-search mcp server

--- src/mcp_servers/web-search/web_search.py
+++ src/mcp_servers/web-search/web_search.py
@@ -1,5 +1,6 @@
 import asyncio
-from crawl4ai import *
+from crawl4ai import AsyncWebCrawler
+from crawl4ai.async_configs import BrowserConfig, CrawlerRunConfig, CacheMode
 from mcp.server.fastmcp import FastMCP
 import json
 import logging
@@ -12,8 +13,10 @@ mcp = FastMCP("web-search")
 @mcp.tool(name = "Web Search")
 async def brave_web_search(
     search_term: str
-    ) -> str:
-    """Tool to search the brave web search and scrap it's results. 
+    ) -> dict:
+    """Tool to search the brave web search and scrape it's results. 
+    This can be used whenever there is not sufficient information on the topic or no
+    other available tool that could help.
     Resulting links can be used to further search the web using
     the deep_search tool.
 
@@ -21,23 +24,52 @@ async def brave_web_search(
         search_term (str): A search query to send to the Brave search engine.
 
     Returns:
-        str: The raw, scraped webpage as markdown, including further links to deep search.
+        dict: A dictionary of the cleaned, scraped webpage as markdown, 
+              including external links and media sources to deep search if necessary.
     """
     
     search_term_parsed = search_term.replace(" ", "+")
     
+    crawl_config = CrawlerRunConfig(
+        # word_count_threshold=100,
+        excluded_tags=['form', 'header'],
+        exclude_external_links=False,
+        process_iframes=True,
+        remove_overlay_elements=True,
+        # cache_mode=CacheMode.ENABLED  # Default BYPASS to crawl unknown weather stations
+    )
+    
     async with AsyncWebCrawler() as crawler:
         result = await crawler.arun(
             url=f"https://search.brave.com/search?q={search_term_parsed}",
+            config=crawl_config
         )
         
-        logger.info(f'### Scraped search engine results (raw):\n\n{result.markdown}')
+        if result.success:
         
-        return result.markdown
+            crawl_dict = {
+                'markdown':result.markdown.fit_markdown, 
+                'links': result.links,
+                'media': result.media
+                }
+            
+            logger.info('SUCCESS')
+            logger.info(f'{crawl_dict}')
+            
+            return crawl_dict
+        
+        else:
+            
+            logger.info('ERROR')
+            logger.info(f'{result.error_message}')
+            
+            return f'Web scraping failed with error {result.error_message}'
+
 
 @mcp.tool(name = "Deep Search")
 async def deep_search(link: str) -> str:
     """Tool to further search links found in the initial search engine search.
+    This is useful whenever the initial search did not provide enough information.
 
     Args:
         link (str): Link from the initial search engine results.
@@ -46,18 +78,39 @@ async def deep_search(link: str) -> str:
         str: The accessed webpage.
     """
     
-    # try:
+    crawl_config = CrawlerRunConfig(
+        excluded_tags=['form', 'header'],
+        exclude_external_links=False,
+        process_iframes=True,
+        remove_overlay_elements=True,
+    )
+
     async with AsyncWebCrawler() as crawler:
         result = await crawler.arun(
             url=link,
+            config=crawl_config
         )
         
-        logger.info(f'### Scraped deep webpage:\n\n{result.markdown}')
+        if result.success:
         
-        return result.markdown
+            crawl_dict = {
+                'markdown':result.markdown.fit_markdown, 
+                'links': result.links,
+                'media': result.media
+                }
+            
+            logger.info('SUCCESS')
+            logger.info(f'{crawl_dict}')
+            
+            return crawl_dict
         
-    # except Exception as e:
-    #     return f"Scrap request failed with error {e}"
+        else:
+            
+            logger.info('ERROR')
+            logger.info(f'{result.error_message}')
+            
+            return f'Web scraping failed with error {result.error_message}'
+
 
 if __name__ == "__main__":
     # TESTING
